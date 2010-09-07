026a9e70db2e96ec7afb9518d0228a1952059b76
Aubort Jean-Baptiste <jean-baptiste.aubort@camptocamp.com>
2010-09-07 15:54:13 +0200

    Put things where it belongs (seperation between CH1903 and Swisstopo implementation)

--- src/com/camptocamp/android/gis/CH1903.java
+++ src/com/camptocamp/android/gis/CH1903.java
@@ -6,7 +6,6 @@ import android.util.Log;
 
 import com.nutiteq.components.MapPos;
 import com.nutiteq.components.Point;
-import com.nutiteq.components.TileMapBounds;
 import com.nutiteq.maps.BaseMap;
 import com.nutiteq.maps.projections.Projection;
 import com.nutiteq.ui.Copyright;
@@ -23,12 +22,12 @@ import com.nutiteq.ui.Copyright;
 public abstract class CH1903 extends BaseMap implements Projection {
 
     private static final String TAG = Map.D + "CH1903";
-    private static final double CH_MIN_X = 420000; // 485869.5728;
-    private static final double CH_MAX_X = 900000; // 837076.5648;
-    private static final double CH_MIN_Y = 30000; // 76443.1884;
-    private static final double CH_MAX_Y = 350000; // 299941.7864;
-    private final HashMap<Integer, Double> resolutions = new HashMap<Integer, Double>();
-    private int zoom;
+    protected static double CH_MIN_X = 485869.5728;
+    protected static double CH_MAX_X = 837076.5648;
+    protected static double CH_MIN_Y = 76443.1884;
+    protected static double CH_MAX_Y = 299941.7864;
+    protected int zoom;
+    protected final HashMap<Integer, Double> resolutions = new HashMap<Integer, Double>();
     protected double ch_pixel_y;
 
     public CH1903(final Copyright copyright, final int tileSize, final int minZoom,
@@ -45,19 +44,18 @@ public abstract class CH1903 extends BaseMap implements Projection {
         setValues(initialZoom, tileSize);
     }
 
-    private void initResolutions() {
-        resolutions.put(14, 650D);
-        resolutions.put(15, 500D);
-        resolutions.put(16, 250D);
-        resolutions.put(17, 100D);
-        resolutions.put(18, 50D);
-        resolutions.put(19, 20D);
-        resolutions.put(20, 10D);
-        resolutions.put(21, 5D);
-        resolutions.put(22, 2.5D);
-        resolutions.put(23, 2D);
-        resolutions.put(24, 0.5D);
-    }
+    // Set resolution for each zoom level in the resolutions HashMap
+    protected abstract void initResolutions();
+
+    protected abstract double CHxtoPIX(double pt);
+
+    protected abstract double CHytoPIX(double pt);
+
+    protected abstract double PIXtoCHx(double px);
+
+    protected abstract double PIXtoCHy(double px);
+
+    protected abstract void setValues(final int zoom, final int tileSize);
 
     public Point mapPosToWgs(MapPos pos) {
         // Convert from CH1903 to pixel
@@ -142,46 +140,10 @@ public abstract class CH1903 extends BaseMap implements Projection {
         return sec + (double) min * 60 + (double) deg * 3600;
     }
 
-    private double CHxtoPIX(double pt) {
-        final double px = (pt - CH_MIN_X) / resolutions.get(zoom);
-        Log.v(TAG, pt + " (CHx) -> " + px + " (PX)");
-        return px;
-    }
-
-    private double CHytoPIX(double pt) {
-        final double px = (CH_MAX_Y - pt) / resolutions.get(zoom);
-        Log.v(TAG, pt + " (CHy) -> " + px + " (PX)");
-        return px;
-    }
-
-    private double PIXtoCHx(double px) {
-        final double pt = CH_MIN_X + (px * resolutions.get(zoom));
-        Log.v(TAG, px + " (PX) -> " + pt + " (CHx)");
-        return pt;
-    }
-
-    private double PIXtoCHy(double px) {
-        final double pt = CH_MAX_Y - (px * resolutions.get(zoom));
-        Log.v(TAG, px + " (PX) -> " + pt + " (CHy)");
-        return pt;
-    }
-
-    private void setValues(final int zoom, final int tileSize) {
-        this.zoom = zoom;
-        ch_pixel_y = CHytoPIX(CH_MIN_Y);
-    }
-
     @Override
     public MapPos zoom(MapPos middlePoint, int zoomSteps) {
         MapPos pos = super.zoom(middlePoint, zoomSteps);
         setValues(pos.getZoom(), getTileSize());
         return pos;
-    }
-
-    @Override
-    public TileMapBounds getTileMapBounds(final int zoom) {
-        final MapPos min = new MapPos((int) CHxtoPIX(CH_MIN_X), (int) CHytoPIX(CH_MAX_Y), zoom);
-        final MapPos max = new MapPos((int) CHxtoPIX(CH_MAX_X), (int) CHytoPIX(CH_MIN_Y), zoom);
-        return new TileMapBounds(min, max);
     }
 }
--- src/com/camptocamp/android/gis/SwisstopoMap.java
+++ src/com/camptocamp/android/gis/SwisstopoMap.java
@@ -1,13 +1,26 @@
 package com.camptocamp.android.gis;
 
+import android.util.Log;
+
+import com.nutiteq.components.MapPos;
+import com.nutiteq.components.TileMapBounds;
 import com.nutiteq.maps.UnstreamedMap;
 
 public class SwisstopoMap extends CH1903 implements UnstreamedMap {
 
-    // private static final String TAG = Map.D + "SwisstopoMap";
+    private static final String TAG = Map.D + "SwisstopoMap";
     private String baseUrl;
     private String format;
     private int tileSize;
+    // private final Random rand = new Random();
+    // private final int MIN = 5;
+    // private final int MAX = 9;
+
+    // Swisstopo data MAX_EXTEND
+    protected static double CH_MIN_X = 420000;
+    protected static double CH_MAX_X = 900000;
+    protected static double CH_MIN_Y = 30000;
+    protected static double CH_MAX_Y = 350000;
 
     public SwisstopoMap(String baseUrl, String format, int tileSize, int minZoom, int maxZoom,
             String copyright, final int initialZoom) {
@@ -18,15 +31,68 @@ public class SwisstopoMap extends CH1903 implements UnstreamedMap {
     }
 
     public String buildPath(int mapX, int mapY, int zoom) {
-        // Log.v(TAG, "mapX=" + mapX + ", mapY=" + mapY);
+        // FIXME: This is supposed to be good :(
+        // double x = mapX / tileSize;
+        // double y = (ch_pixel_y - mapY) / tileSize;
+
         int x = (int) Math.floor(mapX / tileSize);
         int y = (int) Math.floor((ch_pixel_y - mapY) / tileSize);
-        // Log.v(TAG, "x=" + x + ", y=" + y);
-        return String.format(baseUrl, zoom, 0, 0, x, 0, 0, y, format);
-        // return String.format(baseUrl, zoom, (int) (x / 1000000), (int) (x /
-        // 1000) % 1000,
-        // (int) (x % 1000), (int) (y / 1000000), (int) (y / 1000) % 1000, (int)
-        // (y % 1000),
-        // format);
+
+        // int r = rand.nextInt(MAX - MIN + 1) + MIN;
+        int r = 5;
+        return String.format(baseUrl, r, zoom, (int) (x / 1000000), (int) (x / 1000) % 1000,
+                (int) (x % 1000), (int) (y / 1000000), (int) (y / 1000) % 1000, (int) (y % 1000),
+                format);
+    }
+
+    protected void initResolutions() {
+        // Tile resolution for each zoom level
+        resolutions.put(14, 650D);
+        resolutions.put(15, 500D);
+        resolutions.put(16, 250D);
+        resolutions.put(17, 100D);
+        resolutions.put(18, 50D);
+        resolutions.put(19, 20D);
+        resolutions.put(20, 10D);
+        resolutions.put(21, 5D);
+        resolutions.put(22, 2.5D);
+        resolutions.put(23, 2D);
+        resolutions.put(24, 0.5D);
+    }
+
+    protected double CHxtoPIX(double pt) {
+        final double px = (pt - CH_MIN_X) / resolutions.get(zoom);
+        Log.v(TAG, pt + " (CHx) -> " + px + " (PX)");
+        return px;
+    }
+
+    protected double CHytoPIX(double pt) {
+        final double px = (CH_MAX_Y - pt) / resolutions.get(zoom);
+        Log.v(TAG, pt + " (CHy) -> " + px + " (PX)");
+        return px;
+    }
+
+    protected double PIXtoCHx(double px) {
+        final double pt = CH_MIN_X + (px * resolutions.get(zoom));
+        Log.v(TAG, px + " (PX) -> " + pt + " (CHx)");
+        return pt;
+    }
+
+    protected double PIXtoCHy(double px) {
+        final double pt = CH_MAX_Y - (px * resolutions.get(zoom));
+        Log.v(TAG, px + " (PX) -> " + pt + " (CHy)");
+        return pt;
+    }
+
+    protected void setValues(final int zoom, final int tileSize) {
+        this.zoom = zoom;
+        ch_pixel_y = CHytoPIX(CH_MIN_Y);
+    }
+
+    @Override
+    public TileMapBounds getTileMapBounds(final int zoom) {
+        final MapPos min = new MapPos((int) CHxtoPIX(CH_MIN_X), (int) CHytoPIX(CH_MAX_Y), zoom);
+        final MapPos max = new MapPos((int) CHxtoPIX(CH_MAX_X), (int) CHytoPIX(CH_MIN_Y), zoom);
+        return new TileMapBounds(min, max);
     }
 }
