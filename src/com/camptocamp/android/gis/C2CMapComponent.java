d707bb013cdbb6ae422433935576db0348cc17ed
Aubort Jean-Baptiste <jean-baptiste.aubort@camptocamp.com>
2010-10-25 11:03:20 +0200

    Double Tap to zoomIn()

--- src/com/camptocamp/android/gis/C2CMapComponent.java
+++ src/com/camptocamp/android/gis/C2CMapComponent.java
@@ -2,7 +2,6 @@ package com.camptocamp.android.gis;
 
 import android.os.Handler;
 import android.os.Message;
-import android.util.Log;
 import android.view.animation.DecelerateInterpolator;
 
 import com.nutiteq.BasicMapComponent;
@@ -15,9 +14,15 @@ public class C2CMapComponent extends BasicMapComponent {
     private static final String KEY = "182be0c5cdcd5072bb1864cdee4d3d6e4c593f89365962.70956542";
     private static final int MOVE = 0;
     private static final long DELAY = 24; // ms
+    private static final long DOUBLETAP_DELTA = 500; // ms
+    private static final int DOUBLETAP_RADIUS = 50; // px
+    private static final int DOUBLETAP_PAN = 2; // px
 
+    private int lastposx = 0;
+    private int lastposy = 0;
     private int lastpanx = 0;
     private int lastpany = 0;
+    private long lasttouch;
 
     public C2CMapComponent(WgsPoint middlePoint, int zoom) {
         super(KEY, Map.VDR, Map.APP, 1, 1, middlePoint, zoom);
@@ -47,6 +52,7 @@ public class C2CMapComponent extends BasicMapComponent {
     @Override
     public void pointerPressed(final int x, final int y) {
         super.pointerPressed(x, y);
+        // Reset Easing
         mHandler.removeMessages(MOVE);
         posx = 0;
         posy = 0;
@@ -56,8 +62,20 @@ public class C2CMapComponent extends BasicMapComponent {
     @Override
     public void pointerReleased(final int x, final int y) {
         super.pointerReleased(x, y);
-        Message msg = mHandler.obtainMessage(MOVE, lastpanx, lastpany);
-        mHandler.sendMessageDelayed(msg, DELAY);
+
+        // Initiate Easing
+        mHandler.sendMessageDelayed(mHandler.obtainMessage(MOVE, lastpanx, lastpany), DELAY);
+
+        // Double Tap
+        long now = System.currentTimeMillis();
+        if (now - lasttouch <= DOUBLETAP_DELTA && Math.abs(lastposx - x) < DOUBLETAP_RADIUS
+                && Math.abs(lastposy - y) < DOUBLETAP_RADIUS && lastpanx < DOUBLETAP_PAN
+                && lastpany < DOUBLETAP_PAN) {
+            zoomIn();
+        }
+        lasttouch = now;
+        lastposx = x;
+        lastposy = y;
     }
 
     DecelerateInterpolator di = new DecelerateInterpolator(1.0f);
@@ -76,8 +94,8 @@ public class C2CMapComponent extends BasicMapComponent {
                 posx += x;
                 posy += y;
                 current += 0.1;
-                Message msg1 = mHandler.obtainMessage(MOVE, msg.arg1, msg.arg2);
-                mHandler.sendMessageDelayed(msg1, DELAY);
+                mHandler
+                        .sendMessageDelayed(mHandler.obtainMessage(MOVE, msg.arg1, msg.arg2), DELAY);
             }
         }
     };
