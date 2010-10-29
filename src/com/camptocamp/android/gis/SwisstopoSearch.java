ffa78976612f5e343fa1a2a428ac0b0eeaa5ee4b
Aubort Jean-Baptiste <jean-baptiste.aubort@camptocamp.com>
2010-10-29 17:03:20 +0200

    Swisstopo search json query is working

--- src/com/camptocamp/android/gis/SwisstopoSearch.java
+++ src/com/camptocamp/android/gis/SwisstopoSearch.java
@@ -1,5 +1,14 @@
 package com.camptocamp.android.gis;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.microedition.io.Connector;
+import javax.microedition.io.HttpConnection;
+
 import android.app.SearchManager;
 import android.database.Cursor;
 import android.database.MatrixCursor;
@@ -7,19 +16,53 @@ import android.net.Uri;
 import android.provider.BaseColumns;
 import android.util.Log;
 
+import com.nutiteq.utils.IOUtils;
+
 public class SwisstopoSearch extends C2CSearch {
 
     @Override
     public Cursor query(Uri uri, String[] projection, String selection, String[] selectionArgs,
             String sortOrder) {
-        String query = uri.getLastPathSegment().toLowerCase();
-        Log.v("SearchLocation", "query=" + query);
         MatrixCursor answer = new MatrixCursor(new String[] { BaseColumns._ID,
                 SearchManager.SUGGEST_COLUMN_TEXT_1, SearchManager.SUGGEST_COLUMN_INTENT_DATA });
-        answer.addRow(new String[] { "0", "Test0", "Test0" });
-        answer.addRow(new String[] { "1", "Test1", "Test1" });
-        answer.addRow(new String[] { "2", "Test2", "Test2" });
+        String query = uri.getLastPathSegment().toLowerCase();
+        String[] sugg = getSuggestions(query);
+        int i = 0;
+        for (String s : sugg) {
+            answer.addRow(new String[] { "" + i, s, s });
+            i++;
+        }
         return answer;
     }
 
+    private String[] getSuggestions(String query) {
+        List<String> sugg = new ArrayList<String>();
+
+        String url = String.format(getContext().getString(R.string.url_search), System
+                .currentTimeMillis(), query);
+
+        HttpConnection conn = null;
+        BufferedReader in = null;
+        try {
+            conn = (HttpConnection) Connector.open(url, Connector.READ);
+            conn.setRequestMethod(HttpConnection.GET);
+            final int responseCode = conn.getResponseCode();
+            if (responseCode == HttpConnection.HTTP_OK
+                    || responseCode == HttpConnection.HTTP_NOT_MODIFIED) {
+                String json = "";
+                in = new BufferedReader(new InputStreamReader(conn.openInputStream()), 8 * 1024);
+                while ((json = in.readLine()) != null) {
+                    break;
+                }
+                Log.v("", json);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            IOUtils.closeReader(in);
+            IOUtils.closeConnection(conn);
+        }
+
+        return sugg.toArray(new String[sugg.size()]);
+    }
 }
