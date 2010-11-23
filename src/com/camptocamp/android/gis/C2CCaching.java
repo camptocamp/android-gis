72571780b2b2572a459d47e99a514d6e98bac150
Aubort Jean-Baptiste <jean-baptiste.aubort@camptocamp.com>
2010-11-23 16:56:17 +0100

    bigger memorycache size..

--- src/com/camptocamp/android/gis/C2CCaching.java
+++ src/com/camptocamp/android/gis/C2CCaching.java
@@ -18,7 +18,7 @@ public class C2CCaching extends CachingChain {
     private static final String TAG = Map.D + "C2CCaching";
 
     // An image is ~25kB => 1MB = 40 cached images
-    private static final int SCREENCACHE = 8 * 1024; // Bytes
+    private static final int MEMORYCACHE = 1024 * 1024; // Bytes
     public static final File FSCACHEDIR = new File(Environment.getExternalStorageDirectory()
             .getAbsolutePath()
             + "/Android/data/" + Map.PKG);
@@ -33,11 +33,11 @@ public class C2CCaching extends CachingChain {
         if (prefs.getBoolean(Prefs.KEY_FS_CACHING, Prefs.DEFAULT_FS_CACHING)) {
             int size = prefs.getInt(Prefs.KEY_FS_CACHING_SIZE, Prefs.DEFAULT_FS_CACHING_SIZE);
             Log.v(TAG, "fs caching on, size=" + size);
-            cl = new Cache[] { new MemoryCache(SCREENCACHE),
+            cl = new Cache[] { new MemoryCache(MEMORYCACHE),
                     new AndroidFileSystemCache(ctxt, Map.APP, FSCACHEDIR, size) };
         } else {
             Log.v(TAG, "fs caching off");
-            cl = new Cache[] { new MemoryCache(SCREENCACHE) };
+            cl = new Cache[] { new MemoryCache(MEMORYCACHE) };
         }
         return cl;
     }
