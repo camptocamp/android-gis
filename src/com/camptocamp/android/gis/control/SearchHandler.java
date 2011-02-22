37b266b27354f184f799f236ff61c467a0670136
Aubort Jean-Baptiste <jean-baptiste.aubort@camptocamp.com>
2011-02-22 17:05:27 +0100

    Use ASyncTask to do the query

--- src/com/camptocamp/android/gis/control/SearchHandler.java
+++ src/com/camptocamp/android/gis/control/SearchHandler.java
@@ -1,10 +1,14 @@
 package com.camptocamp.android.gis.control;
 
+import java.lang.ref.WeakReference;
+
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import android.app.Activity;
 import android.app.ListActivity;
+import android.app.ProgressDialog;
 import android.app.SearchManager;
 import android.app.AlertDialog.Builder;
 import android.content.DialogInterface;
@@ -12,6 +16,7 @@ import android.content.Intent;
 import android.content.DialogInterface.OnClickListener;
 import android.database.Cursor;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.Toast;
@@ -23,6 +28,8 @@ public abstract class SearchHandler extends ListActivity {
 
     private static final String TAG = BaseMap.D + "SearchHandler";
     private static final String SRC = "content://%1$s.C2CSearch/search_suggest_query/%2$s?limit=50";
+    private ProgressDialog dialog;
+
     public static final String JSON_BBOX = "bbox";
     public static final String JSON_LABEL = "label";
     public static final String JSON_ID = "label";
@@ -39,12 +46,8 @@ public abstract class SearchHandler extends ListActivity {
         // Handle search query (just send the query)
         if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
             query = intent.getStringExtra(SearchManager.QUERY);
-
             if (search != null) {
-                // TODO: use managedQuery()
-                final Cursor c = search.query(Uri.parse(String.format(SRC, BaseMap.PKG, query)),
-                        null, null, null, null);
-                showResult(c);
+                new QueryTask(SearchHandler.this).execute(query);
             }
             else {
                 Toast.makeText(getApplicationContext(), R.string.toast_no_search_provider,
@@ -108,5 +111,38 @@ public abstract class SearchHandler extends ListActivity {
             startActivity(newintent);
             finish();
         }
+    }
+
+    private class QueryTask extends AsyncTask<String, Void, Cursor> {
+
+        private WeakReference<Activity> mActivity;
+
+        public QueryTask(Activity activity) {
+            mActivity = new WeakReference<Activity>(activity);
+        }
+
+        @Override
+        protected void onPreExecute() {
+            final Activity a = mActivity.get();
+            if (a != null) {
+                dialog = ProgressDialog.show(a, "", "FIXME: Searching");
+            }
+        }
+
+        @Override
+        protected Cursor doInBackground(String... query) {
+            return search.query(Uri.parse(String.format(SRC, BaseMap.PKG, query[0])), null, null,
+                    null, null);
+        }
+
+        @Override
+        protected void onPostExecute(Cursor c) {
+            final Activity a = mActivity.get();
+            if (a != null) {
+                dialog.dismiss();
+            }
+            showResult(c);
+        }
+
     }
 }
