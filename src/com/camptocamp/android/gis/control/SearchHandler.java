0f686fc83231c8b205ba0ad79362c3bfea52e987
Aubort Jean-Baptiste <jean-baptiste.aubort@camptocamp.com>
2011-02-23 14:18:47 +0100

    SearchHandler is now abstract and result handling must be implemented elsewhere

--- src/com/camptocamp/android/gis/control/SearchHandler.java
+++ src/com/camptocamp/android/gis/control/SearchHandler.java
@@ -2,18 +2,11 @@ package com.camptocamp.android.gis.control;
 
 import java.lang.ref.WeakReference;
 
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
 import android.app.Activity;
 import android.app.ListActivity;
 import android.app.ProgressDialog;
 import android.app.SearchManager;
-import android.app.AlertDialog.Builder;
-import android.content.DialogInterface;
 import android.content.Intent;
-import android.content.DialogInterface.OnClickListener;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.AsyncTask;
@@ -23,6 +16,8 @@ import android.widget.Toast;
 
 import com.camptocamp.android.gis.BaseMap;
 import com.camptocamp.android.gis.R;
+
+// TODO: Refactor and remove ACTION_GOTO from here (in showResultActivity())
 
 public abstract class SearchHandler extends ListActivity {
 
@@ -34,8 +29,11 @@ public abstract class SearchHandler extends ListActivity {
     public static final String JSON_LABEL = "label";
     public static final String JSON_ID = "label";
 
-    protected SearchProvider search = null; // abstract ?
+    protected SearchProvider search = null;
     protected String query = "";
+
+    abstract protected void showResult(final Cursor c);
+    abstract protected void showResultActivity(String dataString);
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -60,58 +58,58 @@ public abstract class SearchHandler extends ListActivity {
         }
     }
 
-    protected void showResult(final Cursor c) {
-        if (c != null) {
-            if (c.getCount() > 0) {
-                if (c.getCount() == 1) {
-                    c.moveToFirst();
-                    showResultActivity(c.getString(3));
-                    c.close();
-                }
-                else {
-                    Builder alertDialog = new Builder(SearchHandler.this);
-                    alertDialog.setTitle(R.string.dialog_search_results);
-                    alertDialog.setCursor(c, new OnClickListener() {
-                        @Override
-                        public void onClick(DialogInterface dialog, int which) {
-                            showResultActivity(c.getString(3));
-                            c.close();
-                        }
-                    }, SearchManager.SUGGEST_COLUMN_TEXT_1);
-                    alertDialog.create().show();
-                }
-            }
-            else {
-                Toast.makeText(getApplicationContext(), R.string.toast_no_result,
-                        Toast.LENGTH_SHORT).show();
-                finish();
-            }
-        }
-    }
+//    protected void showResult(final Cursor c) {
+//        if (c != null) {
+//            if (c.getCount() > 0) {
+//                if (c.getCount() == 1) {
+//                    c.moveToFirst();
+//                    showResultActivity(c.getString(3));
+//                    c.close();
+//                }
+//                else {
+//                    Builder alertDialog = new Builder(SearchHandler.this);
+//                    alertDialog.setTitle(R.string.dialog_search_results);
+//                    alertDialog.setCursor(c, new OnClickListener() {
+//                        @Override
+//                        public void onClick(DialogInterface dialog, int which) {
+//                            showResultActivity(c.getString(3));
+//                            c.close();
+//                        }
+//                    }, SearchManager.SUGGEST_COLUMN_TEXT_1);
+//                    alertDialog.create().show();
+//                }
+//            }
+//            else {
+//                Toast.makeText(getApplicationContext(), R.string.toast_no_result,
+//                        Toast.LENGTH_SHORT).show();
+//                finish();
+//            }
+//        }
+//    }
 
-    protected void showResultActivity(String jstring) {
-        Intent newintent = new Intent(BaseMap.ACTION_GOTO);
-        try {
-            JSONObject json = new JSONObject(jstring);
-            JSONArray a = json.getJSONArray(JSON_BBOX);
-            newintent.setFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
-            newintent.putExtra(BaseMap.EXTRA_LABEL, json.getString(JSON_LABEL));
-
-            // Bbox
-            newintent.putExtra(BaseMap.EXTRA_MINLON, a.getDouble(0));
-            newintent.putExtra(BaseMap.EXTRA_MINLAT, a.getDouble(1));
-            newintent.putExtra(BaseMap.EXTRA_MAXLON, a.getDouble(2));
-            newintent.putExtra(BaseMap.EXTRA_MAXLAT, a.getDouble(3));
-
-        }
-        catch (JSONException e) {
-            e.printStackTrace();
-        }
-        finally {
-            startActivity(newintent);
-            finish();
-        }
-    }
+//    protected void showResultActivity(String jstring) {
+//        Intent newintent = new Intent(BaseMap.ACTION_GOTO);
+//        try {
+//            JSONObject json = new JSONObject(jstring);
+//            JSONArray a = json.getJSONArray(JSON_BBOX);
+//            newintent.setFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
+//            newintent.putExtra(BaseMap.EXTRA_LABEL, json.getString(JSON_LABEL));
+//
+//            // Bbox
+//            newintent.putExtra(BaseMap.EXTRA_MINLON, a.getDouble(0));
+//            newintent.putExtra(BaseMap.EXTRA_MINLAT, a.getDouble(1));
+//            newintent.putExtra(BaseMap.EXTRA_MAXLON, a.getDouble(2));
+//            newintent.putExtra(BaseMap.EXTRA_MAXLAT, a.getDouble(3));
+//
+//        }
+//        catch (JSONException e) {
+//            e.printStackTrace();
+//        }
+//        finally {
+//            startActivity(newintent);
+//            finish();
+//        }
+//    }
 
     private class QueryTask extends AsyncTask<String, Void, Cursor> {
 
