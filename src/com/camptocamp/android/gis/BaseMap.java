d1439c9730a4c9c6b8c52e3391b952bad0a6e52d
Aubort Jean-Baptiste <jean-baptiste.aubort@camptocamp.com>
2011-02-23 17:38:39 +0100

    More granular GOTO action

--- src/com/camptocamp/android/gis/BaseMap.java
+++ src/com/camptocamp/android/gis/BaseMap.java
@@ -69,6 +69,8 @@ public abstract class BaseMap extends Activity {
     public static final String EXTRA_MSG = "extra_msg";
     public static final String EXTRA_FIELD = "extra_field";
     public static final String EXTRA_COORD = "extra_coord";
+    public static final String EXTRA_LAT = "extra_lat";
+    public static final String EXTRA_LON = "extra_lon";
 
     protected SharedPreferences prefs;
     protected String search_query = "";
@@ -126,7 +128,8 @@ public abstract class BaseMap extends Activity {
                         Toast.makeText(BaseMap.this, R.string.toast_gps_stop, Toast.LENGTH_SHORT)
                                 .show();
                         locationSource.quit();
-                    } else {
+                    }
+                    else {
                         Toast.makeText(BaseMap.this, R.string.toast_gps_start, Toast.LENGTH_SHORT)
                                 .show();
                         mapComponent.setLocationSource(locationSource);
@@ -206,7 +209,8 @@ public abstract class BaseMap extends Activity {
         final int itemid = item.getItemId();
         if (itemid == MENU_PREFS) {
             startActivity(new Intent(BaseMap.this, Prefs.class));
-        } else if (itemid == MENU_RECORD) {
+        }
+        else if (itemid == MENU_RECORD) {
             // Record GPS trace
             final GpsProvider gpsProvider = (GpsProvider) mapComponent.getLocationSource();
             if (gpsProvider != null && gpsProvider.isRecord()) {
@@ -223,12 +227,14 @@ public abstract class BaseMap extends Activity {
                 });
                 dialog.setNegativeButton(R.string.btn_no, null);
                 dialog.show();
-            } else if (isTrackingPosition) {
+            }
+            else if (isTrackingPosition) {
                 gpsProvider.setRecord(true);
                 item.setTitle(R.string.menu_record_stop);
                 item.setIcon(android.R.drawable.ic_media_pause);
             }
-        } else if (itemid == MENU_DIRECTION) {
+        }
+        else if (itemid == MENU_DIRECTION) {
             startActivity(new Intent(BaseMap.this, Directions.class));
         }
         return true;
@@ -270,10 +276,12 @@ public abstract class BaseMap extends Activity {
             if (export != null && (file = export.export(gpsProvider.getTrace())) != "") {
                 Toast.makeText(ctxt, String.format(getString(R.string.toast_trace_saved), file),
                         Toast.LENGTH_LONG).show();
-            } else {
+            }
+            else {
                 Toast.makeText(ctxt, R.string.toast_trace_error, Toast.LENGTH_SHORT).show();
             }
-        } else {
+        }
+        else {
             Toast.makeText(ctxt, R.string.toast_trace_empty, Toast.LENGTH_SHORT).show();
         }
     }
@@ -307,21 +315,31 @@ public abstract class BaseMap extends Activity {
                 }
             });
 
-        } else if (ACTION_TOAST.equals(action)) {
+        }
+        else if (ACTION_TOAST.equals(action)) {
             Toast.makeText(ctxt, intent.getStringExtra(EXTRA_MSG), Toast.LENGTH_SHORT).show();
 
-        } else if (ACTION_GOTO.equals(action)) {
-            search_query = intent.getStringExtra(EXTRA_LABEL);
-            ((TextView) findViewById(R.id.search_query)).setText(search_query);
+        }
+        else if (ACTION_GOTO.equals(action)) {
 
-            // Get positions and Zoom
-            final double minx = intent.getDoubleExtra(EXTRA_MINLON, 0);
-            final double miny = intent.getDoubleExtra(EXTRA_MINLAT, 0);
-            final double maxx = intent.getDoubleExtra(EXTRA_MAXLON, 0);
-            final double maxy = intent.getDoubleExtra(EXTRA_MAXLAT, 0);
-            zoomToBbox(new WgsPoint(minx, miny), new WgsPoint(maxx, maxy));
+            if (intent.hasExtra(EXTRA_LABEL)) {
+                search_query = intent.getStringExtra(EXTRA_LABEL);
+                ((TextView) findViewById(R.id.search_query)).setText(search_query);
 
-        } else if (ACTION_ROUTE.equals(action)) {
+                // Get positions and Zoom
+                final double minx = intent.getDoubleExtra(EXTRA_MINLON, 0);
+                final double miny = intent.getDoubleExtra(EXTRA_MINLAT, 0);
+                final double maxx = intent.getDoubleExtra(EXTRA_MAXLON, 0);
+                final double maxy = intent.getDoubleExtra(EXTRA_MAXLAT, 0);
+                zoomToBbox(new WgsPoint(minx, miny), new WgsPoint(maxx, maxy));
+            }
+            else if (intent.hasExtra(EXTRA_LAT) && intent.hasExtra(EXTRA_LON)) {
+                mapComponent.setMiddlePoint(new WgsPoint(intent.getDoubleExtra(EXTRA_LON, 0),
+                        intent.getDoubleExtra(EXTRA_LAT, 0)));
+            }
+
+        }
+        else if (ACTION_ROUTE.equals(action)) {
             // Modal dialog
             final ProgressDialog dialog = ProgressDialog.show(BaseMap.this,
                     getString(R.string.dialog_route_create_title),
@@ -362,12 +380,14 @@ public abstract class BaseMap extends Activity {
             if (startx < endx && starty > endy) {
                 from = new WgsPoint(startx, endy);
                 to = new WgsPoint(endx, starty);
-            } else if (startx > endx) {
+            }
+            else if (startx > endx) {
                 if (starty > endy) {
                     final WgsPoint tmp = to;
                     to = from;
                     from = tmp;
-                } else {
+                }
+                else {
                     from = new WgsPoint(endx, starty);
                     to = new WgsPoint(startx, endy);
                 }
@@ -399,7 +419,8 @@ public abstract class BaseMap extends Activity {
             for (int i = 0; i < len; i++) {
                 try {
                     layers_names[i] = getString(r.getIdentifier(layers_keys[i], "string", PKG));
-                } catch (Exception e) {
+                }
+                catch (Exception e) {
                     android.util.Log.e(TAG, e.getMessage());
                 }
             }
@@ -418,7 +439,8 @@ public abstract class BaseMap extends Activity {
                         public void onClick(DialogInterface dialog, int which, boolean isChecked) {
                             if (isChecked) {
                                 mSelectedLayers.add(overlay.getLayersAll().get(layers_keys[which]));
-                            } else {
+                            }
+                            else {
                                 mSelectedLayers.remove(overlay.getLayersAll().get(
                                         layers_keys[which]));
                             }
@@ -431,21 +453,24 @@ public abstract class BaseMap extends Activity {
                     if (mSelectedLayers.size() > 0) {
                         overlay.setLayersSelected(TextUtils.join(",", mSelectedLayers.toArray()));
                         gm.addTileOverlay(overlay);
-                    } else {
+                    }
+                    else {
                         gm.addTileOverlay(null);
                     }
                     mapComponent.refreshTileOverlay();
                 }
             });
             dialog.show();
-        } else {
+        }
+        else {
             GeoMap gm = mapComponent.getMap();
             if (mSelectedLayers.size() == 0) {
                 mSelectedLayers.add(PLACEHOLDER);
                 Toast.makeText(BaseMap.this, R.string.toast_overlay_added, Toast.LENGTH_SHORT)
                         .show();
                 gm.addTileOverlay(overlay);
-            } else {
+            }
+            else {
                 mSelectedLayers.remove(PLACEHOLDER);
                 gm.addTileOverlay(null);
                 Toast.makeText(BaseMap.this, R.string.toast_overlay_removed, Toast.LENGTH_SHORT)
