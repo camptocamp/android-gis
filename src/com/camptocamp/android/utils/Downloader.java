deb04b7f07cb41cf2d93ebb648c538c1051bd054
Aubort Jean-Baptiste <jean-baptiste.aubort@camptocamp.com>
2011-04-28 14:07:49 +0200

    Optimizations

--- src/com/camptocamp/android/utils/Downloader.java
+++ src/com/camptocamp/android/utils/Downloader.java
@@ -1,43 +1,36 @@
 package com.camptocamp.android.utils;
 
 import java.io.IOException;
-import java.io.InputStream;
 
 import javax.microedition.io.Connector;
 import javax.microedition.io.HttpConnection;
 
 import android.util.Log;
 
+import com.camptocamp.android.gis.BaseMap;
 import com.nutiteq.utils.IOUtils;
-
-import de.georepublic.android.gis.ui.Main;
 
 public class Downloader {
 
-    public static final String TAG = Main.D + "Downloader";
+    public static final String TAG = BaseMap.D + "Downloader";
     public static final String USERAGENT_KEY = "User-Agent";
     public static final String USERAGENT = "Android GIS (http://camptocamp.com)";
 
     public static String getStringResponse(String url) {
+        String sb;
         HttpConnection conn = null;
-        InputStream is = null;
-        final StringBuffer sb = new StringBuffer();
         try {
             conn = (HttpConnection) Connector.open(url, Connector.READ);
             conn.setRequestProperty(USERAGENT_KEY, USERAGENT);
-            is = conn.openInputStream();
-            int chr;
-            while ((chr = is.read()) != -1) {
-                sb.append((char) chr);
-            }
+            sb = new String(IOUtils.readFullyAndClose(conn.openInputStream()));
         }
         catch (IOException e) {
             Log.e(TAG, e.getMessage());
+            sb = "";
         }
         finally {
-            IOUtils.closeStream(is);
             IOUtils.closeConnection(conn);
         }
-        return sb.toString();
+        return sb;
     }
 }
