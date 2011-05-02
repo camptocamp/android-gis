8770713254de6ad2c7924476ae8807ba6394ed95
Aubort Jean-Baptiste <jean-baptiste.aubort@camptocamp.com>
2011-05-02 10:41:48 +0200

    Don't catch IOException in Downloader

--- src/com/camptocamp/android/utils/Downloader.java
+++ src/com/camptocamp/android/utils/Downloader.java
@@ -5,8 +5,6 @@ import java.io.IOException;
 import javax.microedition.io.Connector;
 import javax.microedition.io.HttpConnection;
 
-import android.util.Log;
-
 import com.camptocamp.android.gis.BaseMap;
 import com.nutiteq.utils.IOUtils;
 
@@ -16,17 +14,13 @@ public class Downloader {
     public static final String USERAGENT_KEY = "User-Agent";
     public static final String USERAGENT = "Android GIS (http://camptocamp.com)";
 
-    public static String getStringResponse(String url) {
+    public static String getStringResponse(String url) throws IOException {
         String sb;
         HttpConnection conn = null;
         try {
             conn = (HttpConnection) Connector.open(url, Connector.READ);
             conn.setRequestProperty(USERAGENT_KEY, USERAGENT);
             sb = new String(IOUtils.readFullyAndClose(conn.openInputStream()));
-        }
-        catch (IOException e) {
-            Log.e(TAG, e.getMessage());
-            sb = "";
         }
         finally {
             IOUtils.closeConnection(conn);
